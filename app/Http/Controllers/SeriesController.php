a0703bef55bbb73f342c12422933eb490965a8d5
Alef Silva <alef_felipe@live.com>
2022-03-04 21:23:50 -0300

    Refatoração

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Serie;
 use App\Http\Requests\SeriesRequest;
+use App\Services\SerieService;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -22,32 +23,24 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function save(SeriesRequest $request)
+    public function save(SeriesRequest $request, SerieService $serieService)
     {
-        $serie = Serie::create(['nome' => $request->nome]);
-
-        $qtdTemporadas = $request->qtd_temporadas;
-
-        $qtdEpisodios = $request->qtd_episodios;
-
-        for ($i = 1; $i <= $qtdTemporadas; $i++) {
-            $temporada = $serie->temporadas()->create(['numero_temporada' => $i]);
-
-            for ($j = 1; $j <= $qtdEpisodios; $j++) {
-                $temporada->episodios()->create(['numero_episodio' => $j]);
-            }
-        }
+        $serie = $serieService->criarSerie(
+            $request->nome,
+            $request->qtd_temporadas,
+            $request->qtd_episodios
+        );
 
         $request->session()->flash('mensagem', "Série $serie->nome criada com sucesso");
 
         return redirect()->route('series.index');
     }
 
-    public function delete(Request $request)
+    public function delete(Request $request, SerieService $serieService)
     {
-        Serie::destroy($request->id);
+        $nomeSerie = $serieService->removerSerie($request->id);
 
-        $request->session()->flash('mensagem', "Série removida com sucesso");
+        $request->session()->flash('mensagem', "Série $nomeSerie removida com sucesso");
 
         return redirect()->route('series.index');
     }
---
+++ app/Services/SerieService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\{Serie, Temporada, Episodio};
+
+class SerieService
+{
+    public function criarSerie(string $nomeSerie, int $qtdTemporadas, int $qtdEpisodios): Serie
+    {
+        DB::beginTransaction();
+            $serie = Serie::create(['nome' => $nomeSerie]);
+            $this->criarTemporada($qtdTemporadas, $qtdEpisodios, $serie);
+        DB::commit();
+
+        return $serie;
+    }
+
+    private function criarTemporada(int $qtdTemporadas, int $qtdEpisodios, Serie $serie): void
+    {
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero_temporada' => $i]);
+            $this->criarEpisodio($qtdEpisodios, $temporada);
+        }
+    }
+
+    private function criarEpisodio(int $qtdEpisodios, Temporada $temporada): void
+    {
+        for ($j = 1; $j <= $qtdEpisodios; $j++) {
+            $temporada->episodios()->create(['numero_episodio' => $j]);
+        }
+    }
+
+    public function removerSerie(int $serieId): string
+    {
+        $nomeSerie = '';
+
+        DB::transaction(function () use ($serieId, &$nomeSerie) {
+            $serie = Serie::find($serieId);
+            $nomeSerie = $serie->nome;
+
+            $this->removerTemporada($serie);
+            $serie->delete();
+        });
+
+        return $nomeSerie;
+    }
+
+    private function removerTemporada(Serie $serie): void
+    {
+        $serie->temporadas->each(function (Temporada $temporada) {
+            $this->removerEpisodio($temporada);
+            $temporada->delete();
+        });
+    }
+
+    private function removerEpisodio(Temporada $temporada): void
+    {
+        $temporada->episodios->each(function (Episodio $episodio) {
+            $episodio->delete();
+        });
+    }
+}
