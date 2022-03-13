b060360445fe0a04553834a209804072b467f21a
Alef Silva <alef_felipe@live.com>
2022-03-13 19:38:45 -0300

    Add make:auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::post('series/{id}/editaNome', 'SeriesController@editaNome');
  */
 Route::get('temporadas/{temporada}/episodios', 'EpisodiosController@index');
 Route::post('temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
