5ff295c43817944b530b577572b19a85c05b27cb
Alef Silva <alef_felipe@live.com>
2022-04-07 16:46:52 -0300

    Update UserSeeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -23,7 +23,47 @@ class UserSeeder extends Seeder
                 'name' => 'Felipe Dantas',
                 'email' => 'felipe@teste.com',
                 'password' => Hash::make('felipe123')
-            ]
+            ],
+            [
+                'name' => 'João Santos',
+                'email' => 'joao@teste.com',
+                'password' => Hash::make('joao123')
+            ],
+            [
+                'name' => 'Flávio Almeida',
+                'email' => 'flavio@teste.com',
+                'password' => Hash::make('flavio123')
+            ],
+            [
+                'name' => 'Pedro Souza',
+                'email' => 'pedro@teste.com',
+                'password' => Hash::make('pedro123')
+            ],
+            [
+                'name' => 'Bruna Oliveira',
+                'email' => 'bruna@teste.com',
+                'password' => Hash::make('bruna123')
+            ],
+            [
+                'name' => 'Jéssica Dias',
+                'email' => 'jessica@teste.com',
+                'password' => Hash::make('jessica123')
+            ],
+            [
+                'name' => 'John Smith',
+                'email' => 'john@teste.com',
+                'password' => Hash::make('john123')
+            ],
+            [
+                'name' => 'Lionel Messi',
+                'email' => 'lionel@teste.com',
+                'password' => Hash::make('lionel123')
+            ],
+            [
+                'name' => 'Larissa Oliveira',
+                'email' => 'larissa@teste.com',
+                'password' => Hash::make('larissa123')
+            ],
         ];
 
         foreach ($usuarios as $usuario) {
