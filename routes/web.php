1ccc7ce4e7028bc2e7b3a694812f95d78194bf06
Alef Silva <alef_felipe@live.com>
2023-07-21 19:40:39 -0300

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -3,60 +3,36 @@
 use Illuminate\Support\Facades\Route;
 
 /**
+ * Root Route
+ */
+Route::redirect('/', 'home');
+
+/**
  * Auth Routes
  */
-Route::get('home', 'HomeController@index')
-    ->name('home');
-
-Route::get('entrar', 'AuthController@index')
-    ->name('login');
-
-Route::post('entrar', 'AuthController@entrar')
-    ->name('login');
-
-Route::get('registrar', 'RegistroController@create')
-    ->name('registro');
-
-Route::post('registrar', 'RegistroController@store')
-    ->name('registro');
-
-Route::get('sair', 'AuthController@logout')
-    ->name('logout');
+Route::get('home', 'HomeController@index')->name('home');
+Route::get('entrar', 'AuthController@index')->name('login');
+Route::post('entrar', 'AuthController@entrar')->name('login');
+Route::get('registrar', 'RegistroController@create')->name('registro');
+Route::post('registrar', 'RegistroController@store')->name('registro');
+Route::get('sair', 'AuthController@logout')->name('logout');
 
 /**
  * Series Routes
  */
-Route::get('series', 'SeriesController@index')
-    ->name('series.index');
-
-Route::get('series/criar', 'SeriesController@create')
-    ->name('series.criar')
-    ->middleware('autenticador');
-
-Route::post('series/criar', 'SeriesController@save')
-    ->name('series.criar')
-    ->middleware('autenticador');
-
-Route::post('series/{id}', 'SeriesController@delete')
-    ->name('series.deletar')
-    ->middleware('autenticador');
-
-Route::post('series/{id}/editaNome', 'SeriesController@editaNome')
-    ->name('series.editar')
-    ->middleware('autenticador');
+Route::get('series', 'SeriesController@index')->name('series.index');
+Route::get('series/criar', 'SeriesController@create')->name('series.criar')->middleware('autenticador');
+Route::post('series/criar', 'SeriesController@save')->name('series.criar')->middleware('autenticador');
+Route::post('series/{id}', 'SeriesController@delete')->name('series.deletar')->middleware('autenticador');
+Route::post('series/{id}/editaNome', 'SeriesController@editaNome')->name('series.editar')->middleware('autenticador');
 
 /**
  * Temporadas Routes
  */
-Route::get('series/{id}/temporadas', 'TemporadasController@index')
-    ->name('temporadas.index');
+Route::get('series/{id}/temporadas', 'TemporadasController@index')->name('temporadas.index');
 
 /**
  * Episodios Routes
  */
-Route::get('temporadas/{temporada}/episodios', 'EpisodiosController@index')
-    ->name('temporadas.index');
-
-Route::post('temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')
-    ->name('temporadas.index')
-    ->middleware('autenticador');
+Route::get('temporadas/{temporada}/episodios', 'EpisodiosController@index')->name('temporadas.index');
+Route::post('temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')->name('temporadas.index')->middleware('autenticador');
